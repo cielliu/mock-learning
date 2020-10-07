10f747c6e73d46a568041692ce2c2f1bae4fd87d
cielliu <ljnbupt@gmail.com>
2020-10-07 16:53:56 +0800

    创建项目，mock未成功

---
+++ src/main.js
@@ -0,0 +1,36 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// /**
+//  * ?????
+//  */
+// // 引入vant
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+
+// 引入axios
+import axios from 'axios'
+
+// // 引入样式文件
+// import '@/common/stylus/index.styl'
+
+// 引入mock文件，正式发布时，注释掉该处即可
+import '@/mock'
+
+Vue.prototype.$axios = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  /**
+   * ????
+   */
+  render: h => h(App)
+  // components: { App },
+  // template: '<App/>'
+})
